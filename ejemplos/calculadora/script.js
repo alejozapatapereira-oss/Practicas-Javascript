f6cb5a4f7fd02d5f14d6771bb35c84df4a27d531
eliasbianchi-uy <elias.bianchi@utec.edu.uy>
2026-04-27 11:33:33 -0300

    Ejercicio para hacer - Calculadora

--- ejemplos/calculadora/script.js
+++ ejemplos/calculadora/script.js
@@ -8,7 +8,7 @@ function resta(numero1, numero2) {
 
 function division(numero1, numero2) {
   if (numero2 == 0) {
-    return 0;
+    return null;
   }
   return numero1 / numero2;
 }
@@ -17,36 +17,4 @@ function multiplicacion(numero1, numero2) {
   return numero1 * numero2;
 }
 
-console.log("7+5 = " + suma(5, 2));
-
-console.log("100 - 70 = " + resta(100, 70));
-
-const dividir = division(9, 3);
-console.log("9 / 3 = " + dividir);
-
-division(100, 0);
-
-console.log("9/3 * 100 = " + multiplicacion(dividir, 100));
-
-function hacerTarea(dato, funcion1) {
-  console.log("empezando tarea..." + dato);
-
-  funcion1(5, 4);
-
-  console.log("finalizando tarea");
-}
-
-hacerTarea(1, (a, b) => {
-  const res = a + b;
-  console.log("haciendo tarea: " + res);
-});
-
-const nombres = ["Agustin", "Lautaro", "Sandra"];
-
-const saludarNombres = nombres.map((nombre) => {
-  if (nombre == "Agustin") return null;
-  return "Hola " + nombre;
-});
-
-console.log(nombres);
-console.log(saludarNombres);
+/* POR HACER: funcionalidad */
